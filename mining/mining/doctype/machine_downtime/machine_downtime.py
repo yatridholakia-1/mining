095b0130c2a43a364c63dbb7978263f3ef6114cf
yatridholakia-1 <yatri@arkayapps.com>
2024-02-23 04:47:30 +0000

    Initial Commit

---
+++ mining/mining/doctype/machine_downtime/machine_downtime.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ArkayApps and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MachineDowntime(Document):
+	pass
