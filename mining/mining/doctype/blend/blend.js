c9c04e20be3d8b5b10c8ad25fee4fe5f94030c65
yatridholakia-1 <yatridholakia-1>
2024-02-26 05:13:51 +0000

    updated blend and material

--- mining/mining/doctype/blend/blend.js
+++ mining/mining/doctype/blend/blend.js
@@ -1,8 +1,25 @@
 // Copyright (c) 2024, ArkayApps and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Blend", {
-// 	refresh(frm) {
+frappe.ui.form.on("Blend", {
+       
+    refresh: function(frm){
+         //Filter in Lumps Table
+            frm.set_query("lumps", "lumps", function(doc, cdt, cdn) {
+                return {
+                    filters: [
+                        ["Material", "material_type", "=", "Lumps"]
+                    ]
+                }
+            });
 
-// 	},
-// });
+            //Filter in Polymer Table
+            frm.set_query("polymer", "polymer", function(doc, cdt, cdn) {
+                return {
+                    filters: [
+                        ["Material", "material_type", "=", "Polymer"]
+                    ]
+                }
+            });
+	}
+});
