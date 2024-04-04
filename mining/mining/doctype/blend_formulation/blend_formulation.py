9bf7b6085850ffed6f5b876d76c69d44e3a5f5b6
yatridholakia-1 <yatridholakia-1>
2024-04-04 10:33:52 +0000

    Blend to Blend Stock Deduct

--- mining/mining/doctype/blend_formulation/blend_formulation.py
+++ mining/mining/doctype/blend_formulation/blend_formulation.py
@@ -5,7 +5,7 @@ import frappe
 from frappe.model.document import Document
 from frappe.utils import nowdate
 from ..api import check_stock_balance, create_stock_entry
-from ..enums import Stock, Stock_Purpose
+from ..enums import Stock, Stock_Purpose, Warehouse
 
 
 class BlendFormulation(Document):
@@ -17,11 +17,10 @@ class BlendFormulation(Document):
 		blend = frappe.get_doc("Blend", self.blend)
 		for row in blend.lumps:
 			qty_to_deduct = row.percentage / 100 * self.quantity
-			#frappe.msgprint(f"{row.lumps} : {qty_to_deduct}")
 			is_qty_available = check_stock_balance(
                     entry_for="Material",
                     entry_item= row.lumps,
-                    warehouse = "Store",
+                    warehouse = Warehouse.STORE.value,
                     check_for_quantity=qty_to_deduct)
             
 			if not is_qty_available:
@@ -33,7 +32,29 @@ class BlendFormulation(Document):
                 stock_entry_item = row.lumps,
                 stock_entry_type = Stock.STOCK_OUT.value,
                 stock_entry_purpose = Stock_Purpose.MATERIAL_CONSUMED.value,
-                source_warehouse = "Store",
+                source_warehouse = Warehouse.STORE.value,
+                quantity = qty_to_deduct,
+                created_from_doc = "Blend Formulation",
+                doc_link = self.name
+            )
+		for row in blend.blend:
+			qty_to_deduct = row.percentage / 100 * self.quantity
+			is_qty_available = check_stock_balance(
+                    entry_for="Blend",
+                    entry_item= row.blend,
+                    warehouse = Warehouse.BLEND_WAREHOUSE.value,
+                    check_for_quantity=qty_to_deduct)
+            
+			if not is_qty_available:
+				frappe.throw(f"Quantity Not Available In Warehouse: '{Warehouse.BLEND_WAREHOUSE.value}' for Blend: '{row.blend}'")
+
+			#Deduct Blend
+			stock_entry = create_stock_entry(
+                stock_entry_for = "Blend",
+                stock_entry_item = row.blend,
+                stock_entry_type = Stock.STOCK_OUT.value,
+                stock_entry_purpose = Stock_Purpose.MATERIAL_CONSUMED.value,
+                source_warehouse = Warehouse.BLEND_WAREHOUSE.value,
                 quantity = qty_to_deduct,
                 created_from_doc = "Blend Formulation",
                 doc_link = self.name
