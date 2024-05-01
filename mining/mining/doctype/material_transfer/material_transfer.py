7e6d3b7040a9cdaecb8795da4b2590cb7f364492
yatridholakia-1 <yatridholakia-1>
2024-05-01 05:11:54 +0000

    material Transfer

--- mining/mining/doctype/material_transfer/material_transfer.py
+++ mining/mining/doctype/material_transfer/material_transfer.py
@@ -4,7 +4,7 @@
 import frappe
 from frappe.model.document import Document
 from frappe.utils import nowdate
-from ..enums import Stock, Stock_Purpose, Material_Transfer_Type
+from ..enums import Stock, Stock_Purpose, Material_Type, Material_Transfer_Type
 from ..api import create_stock_entry, check_stock_balance
 
 
@@ -16,18 +16,18 @@ class MaterialTransfer(Document):
 	def on_submit(self):
 		#Validate Stock is present in source warehosue
 		#Check if balance exists
-		# if self.type == Material_Transfer_Type.MATERIAL_ISSUE.value:
-		# 	validate_transfer(self.batch, self)
+		if self.type == Material_Transfer_Type.MATERIAL_ISSUE.value:
+			validate_transfer(self.batch, self)
 			
 		for row in self.material_transfer:
-				is_qty_available = check_stock_balance(
-					entry_for="Material",
-					entry_item= row.material,
-					warehouse = row.source_warehouse,
-					check_for_quantity=row.quantity)
+				# is_qty_available = check_stock_balance(
+				# 	entry_for="Material",
+				# 	entry_item= row.material,
+				# 	warehouse = row.source_warehouse,
+				# 	check_for_quantity=row.quantity)
 				
-				if not is_qty_available:
-					frappe.throw(f"Quantity Not Available In Warehouse: '{row.source_warehouse}' for Material: '{row.material}'")
+				# if not is_qty_available:
+				# 	frappe.throw(f"Quantity Not Available In Warehouse: '{row.source_warehouse}' for Material: '{row.material}'")
 				
 				stock_transfer_entry = create_stock_entry(
 				stock_entry_for = "Material",
@@ -40,17 +40,17 @@ class MaterialTransfer(Document):
 				created_from_doc = "Material Transfer",
 				doc_link = self.name
 			)
-				row.stock_entry = stock_transfer_entry.name
-				self.save()
+				# row.stock_entry = stock_transfer_entry.name
+				# self.save()
 
 
 	
-	def on_cancel(self):
-		for row in self.material_transfer:
-			try:
-				frappe.get_doc("Stock Management", row.stock_entry).cancel()
-			except Exception as e:
-				frappe.throw(f"Failed to cancel Stock Management Document: {e}")
+	# def on_cancel(self):
+	# 	for row in self.material_transfer:
+	# 		try:
+	# 			frappe.get_doc("Stock Management", row.stock_entry).cancel()
+	# 		except Exception as e:
+	# 			frappe.throw(f"Failed to cancel Stock Management Document: {e}")
 	
 def validate_transfer(batch, doc):
 	"""
@@ -60,7 +60,7 @@ def validate_transfer(batch, doc):
 	for item in doc.material_transfer:
 		found = False
 		for batch_item in batch_doc.batch_materials_required:
-			if item.material_type == batch_item.material_type:
+			if item.material_type == batch_item.material_type and item.material_type != Material_Type.POLYMER.value:
 				if item.material == batch_item.material:
 					found = True
 		if not found:
