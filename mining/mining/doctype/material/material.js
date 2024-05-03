aed0f389063df9c8ecdb4828420210a8537aae57
yatridholakia-1 <yatridholakia-1>
2024-05-03 11:51:40 +0000

    pani

--- mining/mining/doctype/material/material.js
+++ mining/mining/doctype/material/material.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2024, ArkayApps and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Material", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Material", {
+	refresh(frm) {
+        if(frappe.user.has_role("System Manager") || frappe.user.has_role("Managing Director") || frappe.user.has_role("Purchase Manager")){
+                frm.add_custom_button(__("Pack"), function(){
+                    frappe.new_doc('Material Inward', {
+                        'material_code': frm.doc.material,
+                        'material_type': frm.doc.material_type
+                    });
+                }, __("Actions"));
+            }
+        }
+});
